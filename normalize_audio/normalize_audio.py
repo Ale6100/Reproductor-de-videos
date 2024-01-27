d2f097e54918eaa23c63bcf88e2624cb9518f72a
Alejandro Portaluppi <alejandro_portaluppi@outlook.com>
2024-01-27 14:18:03 -0300

    Carpeta normalized generada automáticamente

--- normalize_audio/normalize_audio.py
+++ normalize_audio/normalize_audio.py
@@ -1,10 +1,14 @@
 import os
 
-orginalFile = "../public/videos"
+originalFile = "../public/videos"
+outputDir = "./normalized"
 
-names = [f for f in os.listdir(orginalFile)]
+# Crear la carpeta si no existe
+os.makedirs(outputDir, exist_ok=True)
+
+names = [f for f in os.listdir(originalFile)]
 
 for name in names:
     print(f"Normalizando {name}")
-    os.system(f'ffmpeg-normalize "{orginalFile}/{name}" -o "./normalized/{name}" -c:a aac -b:a 192k --keep-loudness-range-target')
+    os.system(f'ffmpeg-normalize "{originalFile}/{name}" -o "{outputDir}/{name}" -c:a aac -b:a 192k --keep-loudness-range-target')
     print(f"Listo")
